652f6bbc9683980c4d79a38bc5aa68becbae0eb9
fahd <fahdasif51@gmail.com>
2025-07-23 20:00:03 +0500

    DB Changes

--- initDB.js
+++ initDB.js
@@ -42,3 +42,30 @@ export async function initDB() {
       console.log("Error creating patients table")
     }
   }
+
+  
+  export async function initPaitentsDB2()
+  {
+    try
+    {
+        await sql`
+        CREATE TABLE IF NOT EXISTS patients (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            age INT NOT NULL,
+            gravida INT NOT NULL,                   
+            blood_pressure VARCHAR(20),              
+            heighT VARCHAR(20),
+            diabetes BOOLEAN DEFAULT FALSE,          
+            previous_c_section INT NOT NULL,
+
+          );
+      `;
+
+      console.log("Paitents table maded sucessfully")
+    }
+    catch(error)
+    {
+      console.log("Error creating patients table")
+    }
+  }
