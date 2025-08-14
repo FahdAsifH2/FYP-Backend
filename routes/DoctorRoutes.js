0d942f3e4bb671e2a2f7ea557c513a596281da8b
fahd <fahdasif51@gmail.com>
2025-08-15 02:02:25 +0500

    Made the main strcure of api in the backend

--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -1,18 +1,31 @@
-import express from 'express'
-import {sql} from "../db.js"
-import { createPatient } from '../controllers/patientsController.js'
-import { GetAllPatientByName } from '../controllers/patientsController.js'
-import { getPatientDetailsByID } from '../controllers/patientsController.js'
-import { PredictPregnancy } from '../controllers/patientsController.js'
+import express from "express";
+import { sql } from "../db.js";
+import { createPatient } from "../controllers/patientsController.js";
+import { GetAllPatientByName } from "../controllers/patientsController.js";
+import { getPatientDetailsByID } from "../controllers/patientsController.js";
+import { PredictPregnancy } from "../controllers/patientsController.js";
 
+const router = express.Router();
 
-const router =express.Router()
+router.post("/putPatients", createPatient);
+router.get("/getPatientsNames", GetAllPatientByName);
+router.get("/getPatientDetails/:id", getPatientDetailsByID);
+router.get("/putPatients", createPatient);
+router.post("/PredictPregnancy", PredictPregnancy);
 
+router.post("/SubmitAntenatalform", async (req, res) => {
+  console.log("Api end point hitt");
 
-router.post('/putPatients',createPatient)
-router.get('/getPatientsNames',GetAllPatientByName)
-router.get('/getPatientDetails/:id',getPatientDetailsByID)
-router.get('/putPatients',createPatient)
-router.post('/PredictPregnancy',PredictPregnancy)
+  try 
+  {
+    res.status(200).send("ok");
+    const formData = req.body
+    console.log(JSON.stringify(formData,null,2))
+  } catch (error) 
+  {
+    console.log("server error in antenatal",error)
+    res.status(500);
+  }
+});
 
-export default router
+export default router;
